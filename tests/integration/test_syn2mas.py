4b9a19d038caa850dbcf5d2ff70039679007c0ac
Gaël Goinvic <gaelg@element.io>
2025-05-20 15:50:58 +0200

    tests: verify that access tokens work after migration

--- tests/integration/test_syn2mas.py
+++ tests/integration/test_syn2mas.py
@@ -49,28 +49,28 @@ async def test_run_syn2mas_upgrade(
     revision = await helm_client.get_current_revision(
         generated_data.release_name, namespace=generated_data.ess_namespace
     )
-    # values = await revision.values()
-    # values["matrixAuthenticationService"]["syn2mas"]["dryRun"] = False
-    # chart = await helm_client.get_chart("charts/matrix-stack")
-    # # Install or upgrade a release
-    # revision = await helm_client.install_or_upgrade_release(
-    #     generated_data.release_name,
-    #     chart,
-    #     values,
-    #     namespace=generated_data.ess_namespace,
-    #     atomic="CI" not in os.environ,
-    #     wait=True,
-    # )
-    # assert revision.status == pyhelm3.ReleaseRevisionStatus.DEPLOYED
+    values = await revision.values()
+    values["matrixAuthenticationService"]["syn2mas"]["dryRun"] = False
+    chart = await helm_client.get_chart("charts/matrix-stack")
+    # Install or upgrade a release
+    revision = await helm_client.install_or_upgrade_release(
+        generated_data.release_name,
+        chart,
+        values,
+        namespace=generated_data.ess_namespace,
+        atomic="CI" not in os.environ,
+        wait=True,
+    )
+    assert revision.status == pyhelm3.ReleaseRevisionStatus.DEPLOYED
 
-    # # MAS should be available
-    # await test_matrix_authentication_service_graphql_endpoint(ingress_ready, generated_data, ssl_context)
+    # MAS should be available
+    await test_matrix_authentication_service_graphql_endpoint(ingress_ready, generated_data, ssl_context)
 
-    # sync_result = await aiohttp_post_json(
-    #     f"https://synapse.{generated_data.server_name}/_matrix/client/unstable/org.matrix.simplified_msc3575/sync",
-    #     {},
-    #     {"Authorization": f"Bearer {access_token}"},
-    #     ssl_context,
-    # )
+    sync_result = await aiohttp_post_json(
+        f"https://synapse.{generated_data.server_name}/_matrix/client/unstable/org.matrix.simplified_msc3575/sync",
+        {},
+        {"Authorization": f"Bearer {access_token}"},
+        ssl_context,
+    )
 
-    # assert "pos" in sync_result
+    assert "pos" in sync_result
