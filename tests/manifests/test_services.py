276c525e857b0ad7fedb68c7a95008f86f7d7510
Ben Banfield-Zanin <benbz@matrix.org>
2025-06-11 14:23:23 +0100

    Fix Matrix RTC's SFU constructing an invalid Service if given too wide a nodePort range.

--- tests/manifests/test_services.py
+++ tests/manifests/test_services.py
@@ -13,12 +13,22 @@ from .utils import template_id
 async def test_ports_in_services_are_named(templates):
     for template in templates:
         if template["kind"] == "Service":
-            id = template_id(template)
-            assert "ports" in template["spec"], f"{id} does not specify a ports list"
-            assert len(template["spec"]["ports"]) > 0, f"{id} does not include any ports"
-
             port_names = []
             for port in template["spec"]["ports"]:
-                assert "name" in port, f"{id} has a port without a name: {port}"
+                assert "name" in port, f"{template_id(template)} has a port without a name: {port}"
                 port_names.append(port["name"])
-            assert len(port_names) == len(set(port_names)), f"Port names are not unique: {id}, {port_names}"
+            assert len(port_names) == len(set(port_names)), (
+                f"Port names are not unique: {template_id(template)}, {port_names}"
+            )
+
+
+@pytest.mark.parametrize("values_file", services_values_files_to_test)
+@pytest.mark.asyncio_cooperative
+async def test_not_too_many_ports_in_services(templates):
+    for template in templates:
+        if template["kind"] == "Service":
+            assert "ports" in template["spec"], f"{template_id(template)} does not specify a ports list"
+
+            number_of_ports = len(template["spec"]["ports"])
+            assert number_of_ports > 0, f"{template_id(template)} does not include any ports"
+            assert number_of_ports <= 250, f"{template_id(template)} has more than 250 ports"
