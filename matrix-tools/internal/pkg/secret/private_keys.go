9373420f47ee93da587041ea4c5951121e1dfdef
Ben Banfield-Zanin <benbz@element.io>
2025-12-08 14:40:09 +0000

    Make RSA key size explicit

--- matrix-tools/internal/pkg/secret/private_keys.go
+++ matrix-tools/internal/pkg/secret/private_keys.go
@@ -15,7 +15,7 @@ import (
 	"fmt"
 )
 
-func marshallKey(key any) ([]byte, error) {
+func marshallKeyIntoDER(key any) ([]byte, error) {
 	keyBytes, err := x509.MarshalPKCS8PrivateKey(key)
 	if err != nil {
 		return nil, err
@@ -24,29 +24,33 @@ func marshallKey(key any) ([]byte, error) {
 	return keyBytes, nil
 }
 
-func generateRSA(format string) ([]byte, error) {
+func marshallKeyIntoPEM(rsaPrivateKey *rsa.PrivateKey) ([]byte, error) {
+	return pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(rsaPrivateKey),
+		}), nil
+}
+
+func generateRSA4096(format string) ([]byte, error) {
 	rsaPrivateKey, err := rsa.GenerateKey(rand.Reader, 4096)
 	if err != nil {
 		return nil, err
 	}
 	switch format {
 	case "pem":
-		return pem.EncodeToMemory(
-			&pem.Block{
-				Type:  "RSA PRIVATE KEY",
-				Bytes: x509.MarshalPKCS1PrivateKey(rsaPrivateKey),
-			}), nil
+		return marshallKeyIntoPEM(rsaPrivateKey)
 	case "der":
-		return marshallKey(rsaPrivateKey)
+		return marshallKeyIntoDER(rsaPrivateKey)
 	default:
 		return nil, fmt.Errorf("%s key format unsupported", format)
 	}
 }
 
-func generateEcdsaPrime256v1() ([]byte, error) {
+func generateEcdsaPrime256v1DER() ([]byte, error) {
 	ecdsaPrivateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
 		return nil, err
 	}
-	return marshallKey(ecdsaPrivateKey)
+	return marshallKeyIntoDER(ecdsaPrivateKey)
 }
