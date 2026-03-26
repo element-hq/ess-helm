9f52a54f36c5b8295b7e363824e222d8c4d71950
Gaël Goinvic <gaelg@element.io>
2026-03-26 12:38:47 +0100

    set test-suite from values file

--- tests/integration/scripts.py
+++ tests/integration/scripts.py
@@ -1,18 +1,29 @@
 import sys
 from pathlib import Path
+from typing import Annotated, Literal
 
 import typer
 
 HERE = Path(__file__).resolve().parent
 
+existing_test_suites = [file.stem for file in (HERE / "env").glob("*.rc")]
+
 
 def run_tests():
-    def _run_tests(additional_test_values_file: str | None = None):
+    def _run_tests(
+        test_suite: Annotated[Literal[*existing_test_suites], typer.Option()],
+        additional_test_values_file: str | None = None,
+    ):
         import os
 
         import pytest
 
         argv_base_index = 1
+
+        test_values_file = HERE / "env" / f"{test_suite}.rc"
+        os.environ["TEST_VALUES_FILE"] = str(test_values_file)
+        argv_base_index += 2
+
         if additional_test_values_file:
             os.environ["ADDITIONAL_TEST_VALUES_FILE"] = additional_test_values_file
             argv_base_index += 2
