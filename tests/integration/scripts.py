39f3917ab921918ed6d1eab2d26d76cd36feab64
Gaël Goinvic <gaelg@element.io>
2026-03-26 12:29:53 +0100

    allow to run with additional values file

--- tests/integration/scripts.py
+++ tests/integration/scripts.py
@@ -7,13 +7,20 @@ HERE = Path(__file__).resolve().parent
 
 
 def run_tests():
-    def _run_tests(additional_test_value_file: str | None = None):
+    def _run_tests(additional_test_values_file: str | None = None):
+        import os
+
         import pytest
 
-        errcode = pytest.main([str(HERE)] + sys.argv[1:])
+        argv_base_index = 1
+        if additional_test_values_file:
+            os.environ["ADDITIONAL_TEST_VALUES_FILE"] = additional_test_values_file
+            argv_base_index += 2
+
+        errcode = pytest.main([str(HERE)] + sys.argv[argv_base_index:])
         sys.exit(errcode)
 
-    typer.run(_run_tests())
+    typer.run(_run_tests)
 
 
 def setup_cluster():
