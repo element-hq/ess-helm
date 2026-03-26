07ac7b5779b4c35b196915e75af74c60b58cacc7
Gaël Goinvic <gaelg@element.io>
2026-03-26 12:14:34 +0100

    make ess-community-integration-tests a dependency of root project

--- tests/integration/scripts.py
+++ tests/integration/scripts.py
@@ -1,15 +1,19 @@
 import sys
 from pathlib import Path
 
+import typer
+
 HERE = Path(__file__).resolve().parent
 
 
 def run_tests():
+    def _run_tests(additional_test_value_file: str | None = None):
+        import pytest
 
-    import pytest
+        errcode = pytest.main([str(HERE)] + sys.argv[1:])
+        sys.exit(errcode)
 
-    errcode = pytest.main([HERE] + sys.argv[1:])
-    sys.exit(errcode)
+    typer.run(_run_tests())
 
 
 def setup_cluster():
@@ -18,5 +22,5 @@ def setup_cluster():
     import pytest
 
     os.environ["PYTEST_KEEP_CLUSTER"] = "1"
-    errcode = pytest.main([HERE] + ["--env-setup"])
+    errcode = pytest.main([str(HERE)] + ["--env-setup"])
     sys.exit(errcode)
