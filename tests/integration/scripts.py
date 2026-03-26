d96597cfa1b3011f18aac78a3497a7ed1c10ec2a
Gaël Goinvic <gaelg@element.io>
2026-03-26 14:15:56 +0100

    allow to pull a given ess chart version

--- tests/integration/scripts.py
+++ tests/integration/scripts.py
@@ -12,17 +12,52 @@ existing_test_suites = [file.stem for file in (HERE / "env").glob("*.rc")]
 def run_tests():
     def _run_tests(
         test_suite: Annotated[Literal[*existing_test_suites], typer.Option()],
+        pull_chart: bool = False,
+        chart_version: str | None = None,
         keep: bool = False,
         additional_test_values_file: str | None = None,
         args: Annotated[list[str] | None, typer.Argument(allow_dash=True)] = None,
     ):
         import os
+        import subprocess
+        from importlib.metadata import version
 
         import pytest
+        import semver
+        import yaml
         from dotenv import load_dotenv
 
+        __version__ = version("mypackage")
         if not args:
             args = []
+
+        if pull_chart:
+            if not chart_version:
+                # We pull the chart version depending on the python package version as those are synchronized
+                if semver.Version.is_valid(__version__):
+                    chart_version = __version__
+                else:
+                    raise ValueError("--chart-version must be provided when pulling chart against a development build")
+
+            Path("charts").mkdir(exist_ok=True)
+
+            # Call helm pull to get the latest chart version
+            subprocess.run(
+                ["helm", "pull", f"oci://ghcr.io/element-hq/ess-helm/matrix-stack:{chart_version}"],
+                check=True,
+            )
+
+            subprocess.run(["tar", "xvf", f"matrix-stack-{chart_version}.tgz", "-C", "charts"])
+
+        if chart_version:
+            with open("charts/matrix-stack/Chart.yaml") as f:
+                chart_yaml = yaml.safe_load(f)
+                if chart_yaml["version"] != chart_version:
+                    raise ValueError(
+                        f"Chart version {chart_yaml['version']} does not match "
+                        f"expected required version {chart_version}. "
+                        "Maybe you want to pull with --pull-chart?"
+                    )
 
         test_suite_rc = HERE / "env" / f"{test_suite}.rc"
         load_dotenv(test_suite_rc)
