bad2a6f839eca84f729972e41ce19886c03de9cb
Ben Banfield-Zanin <benbz@element.io>
2026-07-31 12:00:23 +0100

    Add 'relabelings' support to ServiceMonitors

--- tests/manifests/test_service_monitors.py
+++ tests/manifests/test_service_monitors.py
@@ -1,5 +1,5 @@
 # Copyright 2024-2025 New Vector Ltd
-# Copyright 2025 Element Creations Ltd
+# Copyright 2025-2026 Element Creations Ltd
 #
 # SPDX-License-Identifier: AGPL-3.0-only
 
@@ -7,11 +7,12 @@ from typing import Any
 
 import pytest
 
-from . import PropertyType, values_files_to_test
+from . import DeployableDetails, PropertyType, values_files_to_test
 from .utils import (
     assert_covers_expected_workloads,
     find_services_matching_selector,
     find_workload_ids_matching_selector,
+    iterate_deployables_parts,
     template_id,
 )
 
@@ -56,3 +57,60 @@ async def test_no_servicemonitors_created_if_no_servicemonitor_crds(values, make
         assert template["kind"] != "ServiceMonitor", (
             f"{template_id(template)} exists but the ServiceMonitor CRD isn't present"
         )
+
+
+@pytest.mark.parametrize("values_file", values_files_to_test)
+@pytest.mark.asyncio_cooperative
+async def test_service_monitor_defaults(templates):
+    for template in templates:
+        if template["kind"] != "ServiceMonitor":
+            continue
+
+        component_name = template["metadata"]["labels"]["app.kubernetes.io/name"]
+        if component_name == "synapse":
+            assert len(template["spec"]["endpoints"][0]["relabelings"]) == 2
+        elif component_name == "redis":
+            assert len(template["spec"]["endpoints"][0]["relabelings"]) == 1
+        else:
+            assert "relabelings" not in template["spec"]["endpoints"][0]
+
+
+@pytest.mark.parametrize("values_file", values_files_to_test)
+@pytest.mark.asyncio_cooperative
+async def test_service_monitors_allow_adding_relabelings(values, make_templates):
+    def assert_relabeling(relabeling: dict[str, Any]):
+        assert relabeling["targetLabel"] == "test_label"
+        assert relabeling["action"] == "replace"
+        assert relabeling["replacement"] == "test_value"
+
+    def sets_relabelings(deployable_details: DeployableDetails):
+        deployable_details.set_helm_values(
+            values,
+            PropertyType.ServiceMonitor,
+            {
+                "relabelings": [
+                    {
+                        "targetLabel": "test_label",
+                        "action": "replace",
+                        "replacement": "test_value",
+                    }
+                ]
+            },
+        )
+
+    iterate_deployables_parts(sets_relabelings, lambda deployable_details: deployable_details.has_service_monitor)
+
+    for template in await make_templates(values):
+        if template["kind"] != "ServiceMonitor":
+            continue
+
+        component_name = template["metadata"]["labels"]["app.kubernetes.io/name"]
+        if component_name == "synapse":
+            assert len(template["spec"]["endpoints"][0]["relabelings"]) == 3
+            assert_relabeling(template["spec"]["endpoints"][0]["relabelings"][2])
+        elif component_name == "redis":
+            assert len(template["spec"]["endpoints"][0]["relabelings"]) == 2
+            assert_relabeling(template["spec"]["endpoints"][0]["relabelings"][1])
+        else:
+            assert len(template["spec"]["endpoints"][0]["relabelings"]) == 1
+            assert_relabeling(template["spec"]["endpoints"][0]["relabelings"][0])
