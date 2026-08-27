9848472058dffd1cd8158f5c343fd1c2c01cccf6
Ben Banfield-Zanin <benbz@element.io>
2026-08-27 10:53:12 +0100

    Test for empty/non-dict annotations blocks at the top-level and on the Pod template too

--- tests/manifests/test_annotations.py
+++ tests/manifests/test_annotations.py
@@ -5,9 +5,35 @@
 
 
 import pytest
+from frozendict import frozendict
 
 from . import values_files_to_test
 from .utils import ALL_WORKLOAD_KINDS, PodTemplateDetails, template_id
+
+
+@pytest.mark.parametrize("values_file", values_files_to_test)
+@pytest.mark.asyncio_cooperative
+async def test_annotations_absent_or_non_empty(templates):
+    for template in templates:
+        if "annotations" in template["metadata"]:
+            assert isinstance(template["metadata"]["annotations"], frozendict), (
+                f"{template_id(template)} has top-level annotations that isn't a dictionary"
+            )
+            assert len(template["metadata"]["annotations"]) > 0, (
+                f"{template_id(template)} has empty top-level annotations rather than being absent"
+            )
+
+        if template["kind"] in ALL_WORKLOAD_KINDS:
+            pod_template_details = PodTemplateDetails(template)
+
+            if "annotations" in pod_template_details.pod_template["metadata"]:
+                pod_annotations = pod_template_details.pod_template["metadata"]["annotations"]
+                assert isinstance(pod_annotations, frozendict), (
+                    f"{template_id(template)} has pod annotations that isn't a dictionary"
+                )
+                assert len(pod_annotations) > 0, (
+                    f"{template_id(template)} has empty pod annotations rather than being absent"
+                )
 
 
 @pytest.mark.parametrize("values_file", values_files_to_test)
