29c2ad017c28fda2dbe977942aafe7fd45421886
Ben Banfield-Zanin <benbz@element.io>
2026-08-27 10:52:46 +0100

    Fix annotations test not looking at Pod annotations

--- tests/manifests/test_annotations.py
+++ tests/manifests/test_annotations.py
@@ -7,7 +7,7 @@
 import pytest
 
 from . import values_files_to_test
-from .utils import ALL_WORKLOAD_KINDS, template_id
+from .utils import ALL_WORKLOAD_KINDS, PodTemplateDetails, template_id
 
 
 @pytest.mark.parametrize("values_file", values_files_to_test)
@@ -22,7 +22,8 @@ async def test_annotations_dont_contain_things_that_should_be_labels(templates):
         )
 
         if template["kind"] in ALL_WORKLOAD_KINDS:
-            pod_annotations = template["metadata"].get("annotations", {})
+            pod_template_details = PodTemplateDetails(template)
+            pod_annotations = pod_template_details.pod_template["metadata"].get("annotations", {})
             our_pod_annotations = [key for key in pod_annotations if "k8s.element.io" in key]
             assert len(our_pod_annotations) == 0, (
                 f"{template_id(template)} has {our_pod_annotations=} in its Pod spec. "
